7bc0b69cc4fc6c7b097f44987e3dfbd724203f6f
Celso Silva <silva.celso29@gmail.com>
2019-01-18 18:25:26 -0500

    added a nav partial and show index

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,16 @@
 class RoomsController < ApplicationController
+before_action :rooms
+
   def index
+    @rooms = Room.all
   end
 
   def show
+  end
+
+  private
+
+  def rooms
+    @rooms ||= Room.all
   end
 end
